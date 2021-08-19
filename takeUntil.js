b1df8fc69b2fd693ea3ac55351a7c55cdf77fd65
Babak Shirvani <bk.shirvani@gmail.com>
2021-08-19 22:44:50 +0000

    takeUntil.js added

---
+++ takeUntil.js
@@ -0,0 +1,36 @@
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  return Array.isArray(arr1) &&
+    Array.isArray(arr2) &&
+    arr1.length === arr2.length &&
+    arr1.every((element, index) => element === arr2[index]);
+};
+
+
+const takeUntil = function(array, callback) {
+  for (const item of array) {
+    if (callback(item)) {
+      return array.slice(0, array.indexOf(item))
+    }
+  }
+
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", 'been', 'to', 'Hollywood']);
